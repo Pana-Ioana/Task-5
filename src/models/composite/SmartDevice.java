98858adc620bf07cfbaabac47d1c8e3660001c4d
alexandraopritescu <alexandra.opritescu100@gmail.com>
2025-04-28 16:56:38 +0300

    Completare partea de composite- SmartDevice si SmartRoom

--- src/models/composite/SmartDevice.java
+++ src/models/composite/SmartDevice.java
@@ -4,8 +4,8 @@ import models.interfaces.IOTDevice;
 
 public class SmartDevice implements IOTDevice {
 
-    private String name;
-    private int consumption;
+    private final String name;
+    private final int consumption;
 
     public SmartDevice(String name, int consumption) {
         this.name = name;
@@ -14,11 +14,11 @@ public class SmartDevice implements IOTDevice {
 
     @Override
     public void identify() {
-
+        System.out.printf("Dispozitiv «%s» consumă %dkWh%n", name, consumption);
     }
 
     @Override
     public int getConsumption() {
-        return 0;
+        return consumption;
     }
 }
--- src/models/composite/SmartRoom.java
+++ src/models/composite/SmartRoom.java
@@ -1,4 +1,34 @@
 package models.composite;
 
-public class SmartRoom {
+import models.interfaces.IOTDevice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartRoom implements IOTDevice {
+    private final String name;
+    private final List<IOTDevice> children = new ArrayList<>();
+
+    public SmartRoom(String name) {
+        this.name = name;
+    }
+
+    public void addComponent(IOTDevice component) {
+        children.add(component);
+    }
+
+    @Override
+    public void identify() {
+        System.out.printf("Camera «%s» consum total: %dkWh%n", name, getConsumption());
+        for (IOTDevice c : children) {
+            c.identify();
+        }
+    }
+
+    @Override
+    public int getConsumption() {
+        return children.stream()
+                .mapToInt(IOTDevice::getConsumption)
+                .sum();
+    }
 }
